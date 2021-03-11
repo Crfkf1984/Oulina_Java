ef144f3ded99e50f81912ec76265fdec307f13f3
Сергей <seirosso1@gmail.com>
2021-03-11 21:48:29 +0300

    S01-T02

--- src/s01/Player.java
+++ src/s01/Player.java
@@ -6,88 +6,46 @@ public class Player {
     Player player;
     private String nik;
     private int level;
-    private String sex;
+    private Sex sex;
     private int costGold;
-    private List<String> backpack;
+    private List<Backpack>  backpack;
 
-    public Player(String nik, int level){
-
+     Player(String nik){
+       this.nik = nik;
     };
 
-    public Player(String nik, int level, String sex, int costGold, List<String> backpack,
-                  String pen, String sheet, String pencil, String rubber) {
-        this.nik = nik;
-        this.level = level;
-        this.sex = sex;
-        this.costGold = costGold;
-        this.backpack = backpack;
-    }
-
-    public String getNik() {
-        return nik;
-    }
-
-    public void setNik(String nik) {
-        this.nik = nik;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public int getCostGold() {
-        return costGold;
-    }
-
-    public void setCostGold(int costGold) {
-        this.costGold = costGold;
-    }
-
-    public List<String> getBackpack() {
-        return backpack;
-    }
-
-    public void setBackpack(List<String> backpack) {
-        this.backpack = backpack;
-    }
-
-    public void createPlayer(String nik, int level) {
-       player = new Player("Run" , 1);
-    }
+    public Player createPlayer(String nik){
+        return new Player(nik);
+    };
 
     public void increaseLevel() {
-        int level = player.getLevel();
-        level++;
-        player.setLevel(level);
+       level++;
     }
 
     public void chengecostGold(int costGold) {
-        int res  = player.getCostGold();
-        res +=costGold;
-        player.setCostGold(res);
+        this.costGold +=costGold;
     }
 
     public void addBackpack(String item) {
-        backpack.add(item);
+
+            backpack.add(Backpack.valueOf(item));
     }
 
     public void deleteBackpack(String item) {
-        backpack.remove(item);
+       backpack.remove(item);
     }
 
-    public static void main(String[] args) {
-
+     enum Backpack{
+        PEN,
+        PAPER,
+        WATER,
+        EAT
     }
+
+    enum Sex{
+        М,
+        МEN,
+        МENS
+    }
+
 }
