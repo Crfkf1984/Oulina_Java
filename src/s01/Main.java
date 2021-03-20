18398087843436fa7fafffbd451a7ec127ccd1fa
Сергей <seirosso1@gmail.com>
2021-03-20 19:05:18 +0300

    S01-T07

--- src/s01/Main.java
+++ src/s01/Main.java
@@ -2,27 +2,41 @@ package s01;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.function.BinaryOperator;
 
 public class Main {
     public static void main(String[] args) {
-        Player player = Player.createPlayer("Santa");
-        Armor armor = new Armor("Lite", "Black");
-        Armor armor1 = new Armor("Average", "Grey");
-        Armor armor2 = new Armor("Hard", "White");
-        Weapon weapon = new Weapon("Меч", "Простой");
-        Weapon weapon1 = new Weapon("Пика", "Резная");
-        List<Item> items = new ArrayList<>();
-        Item item = Item.createItem("Book");
-        items.add(Item.createItem("Notebook"));
-        items.add(Item.createItem("Shovel"));
-        items.add(Item.createItem("Scrap"));
-        items.add(Item.createItem("Pick"));
-        items.add( Item.createItem("Rock"));
-        items.add(Item.createItem("Stick"));
-        items.add( Item.createItem("Broom"));
-        items.add( Item.createItem("Knife"));
-        items.add( Item.createItem("Saw"));
-        player.addBackpack(item); // TODO: 15.03.2021 как добавить конкретные предметы
+        char h = '+'; char t = '-'; char j = '/'; char r = '*';
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первую переменную!");
+        int a = scanner.nextInt();
+        System.out.println("Введите вторую переменную!");
+        int d = scanner.nextInt();
+        System.out.println("Введите знак выражения!");
+        char c = scanner.next().charAt(0);
+        System.out.println("Подождите выполняется операция ,,,");
+
+        if (h == c) {
+            BinaryOperator<Integer> binaryOperator = (x, y) -> x + y;
+            int e = binaryOperator.apply(a, d);
+            System.out.println(e);
+        } else if (r == c){
+            BinaryOperator<Integer> binaryOperator = (x, y) -> x * y;
+            int g = binaryOperator.apply(a, d);
+            System.out.println(g);
+        } else if (t == c){
+            BinaryOperator<Integer> binaryOperator = (x, y) -> x - y;
+            int k = binaryOperator.apply(a, d);
+            System.out.println(k);
+        } else if (j == c){
+            BinaryOperator<Integer> binaryOperator = (x, y) -> x / y;
+            int f = binaryOperator.apply(a, d);
+            System.out.println(f);
+        } else {
+            System.out.println("Символ не корректен!");
+        }
+        scanner.close();
     }
 
 }
