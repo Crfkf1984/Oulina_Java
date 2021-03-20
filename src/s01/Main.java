63f276cb615d243c8eabd1cddfc01c278b98e665
Сергей <seirosso1@gmail.com>
2021-03-20 21:07:06 +0300

    S01-T08

--- src/s01/Main.java
+++ src/s01/Main.java
@@ -2,27 +2,17 @@ package s01;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        Player player = Player.createPlayer("Santa");
-        Armor armor = new Armor("Lite", "Black");
-        Armor armor1 = new Armor("Average", "Grey");
-        Armor armor2 = new Armor("Hard", "White");
-        Weapon weapon = new Weapon("Меч", "Простой");
-        Weapon weapon1 = new Weapon("Пика", "Резная");
-        List<Item> items = new ArrayList<>();
-        Item item = Item.createItem("Book");
-        items.add(Item.createItem("Notebook"));
-        items.add(Item.createItem("Shovel"));
-        items.add(Item.createItem("Scrap"));
-        items.add(Item.createItem("Pick"));
-        items.add( Item.createItem("Rock"));
-        items.add(Item.createItem("Stick"));
-        items.add( Item.createItem("Broom"));
-        items.add( Item.createItem("Knife"));
-        items.add( Item.createItem("Saw"));
-        player.addBackpack(item); // TODO: 15.03.2021 как добавить конкретные предметы
+        System.out.println("Введите пин код!");
+        Scanner scanner = new Scanner(System.in);
+        String res = scanner.nextLine();
+        Predicate predicate = (x) -> x.equals(4) || x.equals(6);
+        boolean rsl = predicate.test(res.length());
+        System.out.println(rsl);
     }
 
 }
