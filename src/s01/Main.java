7b59126ddb4c38a1497a4b37fe0fe45ecd3ef11b
Сергей <seirosso1@gmail.com>
2021-03-27 13:39:06 +0300

    P05-T05

---
+++ src/s01/Main.java
@@ -0,0 +1,21 @@
+package s01;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Integer> h = new HashSet<>();
+        h.add(1); h.add(2); h.add(3); h.add(-9);
+        h.add(-7);
+        Iterator<Integer> iterator = h.iterator();
+        while (iterator.hasNext()) {
+            Integer res = iterator.next();
+            if (res >= 0) {
+                System.out.println(res);
+            }
+        }
+    }
+}
