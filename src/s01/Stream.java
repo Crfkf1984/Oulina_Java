30410440145b84d917a1f38cdeea365064580f5c
Сергей <seirosso1@gmail.com>
2021-04-01 22:35:32 +0300

    P06-T03

--- src/s01/Stream.java
+++ src/s01/Stream.java
@@ -3,17 +3,20 @@ package s01;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Stream {
     public static void main(String[] args) throws IOException {
-        Collection<String> collection = Arrays.asList("a1", "a2", "a3");
-        //Stream<String> streamFromValues = Stream.of("a1", "a2", "a3");
-        //System.out.println(streamFromValues);
-        //Stream<String> streamFromFiles = Files.lines(Paths.get("src/Lhrn"));
-        IntStream streamFromString = "123".chars();
-        System.out.println(streamFromString);
+        Collection<String> arrayList = new ArrayList<>(Arrays.asList("Valentin", "vasya",
+                "goga", "Vasya", "Petya"));
+         System.out.println(arrayList.stream().distinct().collect(Collectors.toList()));
+         System.out.println(arrayList.stream().distinct().limit(2).collect(Collectors.toList()));
+          arrayList.stream().distinct().filter(x -> x.length() > 5).forEach(System.out::println);
+          System.out.println(arrayList.stream().map(String::toUpperCase).collect(Collectors.toList()));
     }
 }
